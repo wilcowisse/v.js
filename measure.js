2ca7384b35b33987ca6b64146e157112955a4105
Wilco Wisse <wilcowisse@server.fake>
2014-05-16 16:28:36 +0200

    started with feature selection procedure

--- measure.js
+++ measure.js
@@ -1,2 +1,22 @@
 
 
+function ExpressionMeter(){
+	this.size = 10;
+	this.measure=function(node){
+	
+	}
+}
+
+
+measure('Program.body/Function.body/WhileStatement.test', meter)
+
+function measure(){
+	
+}
+
+
+
+measurements['WhileStatement'] = function() {
+	
+}
+
